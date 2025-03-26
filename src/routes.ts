a564970e36872d309f26d77a54036be744a12226
liam-pulsation <liam@pulsation.eco>
2025-03-26 16:13:47 +0100

    refactor: final for publish

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,10 @@ import router from '@adonisjs/core/services/router'
 import { adminConfigService } from './services/admin_config_service.js'
 const ResourceController = () => import('./controllers/resource_controller.js')
 
-export function renderAdminRoutes(prefix: string = 'admin') {
+export function renderAdminRoutes() {
   const adminConfig = adminConfigService.getConfig()
-  return router
-    .group(() => {
-      router.any('/*', [ResourceController, 'index']).as('resource')
-      router.on('').redirectToPath(adminConfig.defaultPath).as('redirect')
-    })
-    .prefix(prefix)
-    .as(prefix)
+  return router.group(() => {
+    router.any('/*', [ResourceController, 'index']).as('resource')
+    router.on('').redirectToPath(adminConfig.defaultPath).as('redirect')
+  })
 }
